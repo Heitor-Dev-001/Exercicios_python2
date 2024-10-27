f0174cd18f467e48ff982d75f7089cd1b220e106
Heitor Silva <heitorsilvanascimento@hotmail.com>
2024-10-27 15:36:29 -0300

    Add files via upload
    Exercício de número 2.
    2° Crie um script em Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.

---
+++ Desafio1.py
@@ -0,0 +1,5 @@
+
+"Crie um script python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado."
+
+nome=input('Qual é o seu nome?')
+print('Olá',nome,'prazer em te conhecer')
